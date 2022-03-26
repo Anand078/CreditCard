f8013176ce18ae277841bfbc847289df6d2d5900
Anand <anand@deftouch.co>
2022-03-26 11:02:21 +0530

    added mongodb store

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import path from 'path';
 import express from 'express';
 import bodyParser from 'body-parser';
 import fs from 'fs';
+import { default as mongodb } from 'mongodb';
+let MongoClient = mongodb.MongoClient;
 
 const app = express();
 const PORT = 5000;
@@ -59,24 +61,31 @@ fetch('https://services.crif-online.ch/CrifOnlineAPI/reports/personAddress', {
     // var lName = data?.decisionMatrix.lastName;
     var score = data?.decisionMatrix.decisionItems[0].value;
     var insertData = '\n' + req.body.firstName + ', ' + req.body.lastName + ', Score: ' + score + 
-    ', ' + req.body.address + ', ' + req.body.email
-    ;
+    ', ' + req.body.address + ', ' + req.body.email;
+
     var textColor = '';
     var text = '';
     if (score > 420){
       textColor = 'green'; 
       text = 'Your credit card is powerful.';
-      fs.appendFile('userdata.txt',insertData, 'utf8',
-    function(err) {     
-        if (err) throw err;
-        console.log("Data is appended to file successfully.")
-});
     }
     else {
       textColor = 'red';
       text = 'Your credit card is not powerful.';
       console.log("Score is : " + score);
     }
+    var url = "mongodb+srv://anand:anand@cluster0.w5unn.mongodb.net/userdb?retryWrites=true&w=majority";  
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("userdb");
+    var myobj = {firstName: req.body.firstName, lastName: req.body.lastName , creditScore: score, address: req.body.address, email: req.body.email};
+    dbo.collection("userdata").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      console.log("1 document inserted");
+      db.close();
+    });
+  });
       res.setHeader("Content-Type", "text/html")
       res.send(`<h1>CREDIT CARD REPORT</h1><p style="color:${textColor}; font-size:45px;"> ${text}</p>`)
   })
@@ -86,3 +95,4 @@ fetch('https://services.crif-online.ch/CrifOnlineAPI/reports/personAddress', {
 app.listen(process.env.PORT || 5000, () => {
   console.log('Our express server is up on port ' + PORT);
 });
+
